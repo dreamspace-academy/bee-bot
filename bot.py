531bfaed3491226c9e857d712ab2addf0c030e9e
ratheesk <ratheesreo@gmail.com>
2023-09-15 17:07:33 +0530

    feat: add functions to chat with text data

---
+++ bot.py
@@ -0,0 +1,48 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+
+def get_text_from_file(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            text = f.read()
+        return text
+    except FileNotFoundError:
+        print("Error: The specified file was not found.")
+        return ""
+    except Exception as e:
+        print(f"Error reading the file: {e}")
+        return ""
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
